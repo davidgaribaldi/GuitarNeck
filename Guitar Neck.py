198b7ca72ed996b4da6604a369c04b64f76326d8
davidgaribaldi <44825671+davidgaribaldi@users.noreply.github.com>
2019-03-06 15:59:46 -0800

    Add files via upload
    Just a little dictionary to help memorize the notes on a guitar neck

---
+++ Guitar Neck.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 13:07:28 2019
+
+@author: DavidGaribaldi
+"""
+
+fretboard = {}
+fretboard['E'] = ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
+fretboard['A'] = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A']
+fretboard['D'] = ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D']
+fretboard['G'] = ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
+fretboard['B'] = ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+fretboard['e'] = ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']        
+print(fretboard['B'][6])         
